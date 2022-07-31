5bdb6e25d55529e48b24c7358249bed82806c656
Aosmiles <arlin.smiles@gmail.com>
2022-07-30 17:44:19 -0700

    test out displaying random images

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+﻿const plane = document.querySelector('.plane')
+const container = document.querySelector(".plane")
+
+let planeImageDir = "images/allies/"
+
+let randomIndex = Math.floor(Math.random() * 18) + 1;
+
+let planeImageSrc = `${planeImageDir}allies_plane_${randomIndex.toString().padStart(3, "0")}.jpg`
+
+console.log(planeImageSrc)
+
+const imageElement = document.createElement('img');
+imageElement.src = planeImageSrc;
+imageElement.style.transform = 'rotate(90deg)'
+plane.append(imageElement);
+
